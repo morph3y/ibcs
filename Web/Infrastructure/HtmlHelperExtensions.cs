243099512b2c0da660b00a3d7d3dea054a7fb5ac
Alex <self@alexdenysenko.com>
2015-04-15 20:57:14 -0400

    moved css, worked on field render

---
+++ Web/Infrastructure/HtmlHelperExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Web.Infrastructure
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString FieldFor<TModel, TType>(this HtmlHelper helper, Expression<Func<TModel, TType>> @for) where TModel : class
+        {
+            var expression = (MemberExpression)@for.Body;
+            string name = expression.Member.Name;
+            var modelInstance = helper.ViewData.Model as TModel;
+            if (modelInstance != null)
+            {
+                helper.ViewContext.Writer.Write("<div class=\"field\">");
+                {
+                    helper.ViewContext.Writer.Write(helper.Label(name));
+                    helper.ViewContext.Writer.Write("<div class=\"value\">");
+                    helper.ViewContext.Writer.Write(helper.Editor(name));
+                    helper.ViewContext.Writer.Write("</div>");
+                }
+                helper.ViewContext.Writer.Write("</div>");
+            }
+            return null;
+        }
+    }
+}
