565ea788217808057508075bd33affb49524ec01
Alex <self@alexdenysenko.com>
2015-04-25 21:29:35 -0400

    added admin area + latest versions of dependencies

---
+++ Web/Areas/Admin/Controllers/TournamentController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Areas.Admin.Controllers
+{
+    public class TournamentController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
